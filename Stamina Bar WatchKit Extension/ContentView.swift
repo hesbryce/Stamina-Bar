da5c7f811acb4f1230c7196a36bab619d2be862b
Zachary Bryce Ellis <zbellis03@gmail.com>
2022-08-24 21:38:00 -0500

    Initial Commit

---
+++ Stamina Bar WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stamina Bar WatchKit Extension
+//
+//  Created by Zachary Ellis on 8/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stamina Bar WatchKit Extension/Stamina_BarApp.swift
@@ -0,0 +1,19 @@
+//
+//  Stamina_BarApp.swift
+//  Stamina Bar WatchKit Extension
+//
+//  Created by Zachary Ellis on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Stamina_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
